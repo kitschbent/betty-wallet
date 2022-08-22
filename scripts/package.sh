78f66ac0fbdf42eeeb528da51143a5763791b75a
kitschbent <34662130+kitschbent@users.noreply.github.com>
2022-08-22 17:27:54 -0400

    Update package.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,17 +5,17 @@ CSP_POLICY="default-src 'self'; style-src 'self' 'sha256-GhG3bE0iJoXJDtzwjDYe4ew
 sed -i.tmp "s/{{cspValue}}/${CSP_POLICY}/g" dist/app.html
 rm dist/app.html.tmp
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../"
-NODE_ENV=production $DIR/node_modules/.bin/webpack --config ./configs/webpack.config.renderer.prod.babel.js
-$DIR/node_modules/.bin/babel $DIR/app/main.js -o ./dist/main.js
-$DIR/node_modules/.bin/babel $DIR/app/menu.js -o ./dist/menu.js
-$DIR/node_modules/.bin/babel $DIR/app/sentry.js -o ./dist/sentry.js
-$DIR/node_modules/.bin/babel $DIR/app/mainWindow.js -o ./dist/mainWindow.js
-$DIR/node_modules/.bin/babel $DIR/app/preload.js -o ./dist/preload.js
-$DIR/node_modules/.bin/babel $DIR/app/background -d ./dist/background
-$DIR/node_modules/.bin/babel $DIR/app/constants -d ./dist/constants
-$DIR/node_modules/.bin/babel $DIR/app/db -d ./dist/db
-$DIR/node_modules/.bin/babel $DIR/app/utils -d ./dist/utils
-$DIR/node_modules/.bin/babel $DIR/app/ducks/walletReducer.js -o ./dist/ducks/walletReducer.js
-$DIR/node_modules/.bin/babel $DIR/app/ducks/nodeReducer.js -o ./dist/ducks/nodeReducer.js
-$DIR/node_modules/.bin/babel $DIR/app/ducks/hip2Reducer.js -o ./dist/ducks/hip2Reducer.js
-$DIR/node_modules/.bin/babel $DIR/app/ducks/claims.js -o ./dist/ducks/claims.js
+NODE_ENV=production https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/webpack --config ./configs/webpack.config.renderer.prod.babel.js
+https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts$DIR/app/main.js -o ./dist/main.js
+https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts/app/menu.js -o ./dist/menu.js
+https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts/app/sentry.js -o ./dist/sentry.js
+https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts/app/mainWindow.js -o ./dist/mainWindow.js
+https://github.com/kyokan/bob-wallet/tree/master/scripts$DIR/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts$DIR/app/preload.js -o ./dist/preload.js
+https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts$DIR/app/background -d ./dist/background
+https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts/app/constants -d ./dist/constants
+https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts/app/db -d ./dist/db
+https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts/app/utils -d ./dist/utils
+https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts/app/ducks/walletReducer.js -o ./dist/ducks/walletReducer.js
+https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts/app/ducks/nodeReducer.js -o ./dist/ducks/nodeReducer.js
+https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts$DIR/app/ducks/hip2Reducer.js -o ./dist/ducks/hip2Reducer.js
+https://github.com/kyokan/bob-wallet/tree/master/scripts/node_modules/.bin/babel https://github.com/kyokan/bob-wallet/tree/master/scripts/app/ducks/claims.js -o ./dist/ducks/claims.js
